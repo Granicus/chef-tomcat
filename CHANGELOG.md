ed6050176d3d06409eaac8a5fdae88e327f023ee
Matt Kasa <mkasa@baent.net>
2013-07-30 13:01:00 -0700

    Bump version to 0.12.11 - set sane tomcat defaults and add support for amazon linux

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.12.11:
+
+* set sane tomcat defaults and add support for amazon linux
+
 ## v0.10.4:
 
 * [COOK-1110] - remove deprecated (by upstream) jpackage recipe
--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,13 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures tomcat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.12.10"
+version          "0.12.11"
 
 %w{ java logrotate ark }.each do |cb|
   depends cb
 end
 
-%w{ debian ubuntu centos redhat fedora }.each do |os|
+%w{ debian ubuntu centos redhat fedora amazon }.each do |os|
   supports os
 end
 
